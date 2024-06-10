d1333f4920dd2b4b41cd871e40b7d2367bbbd14d
LXSCA <lucas.edr27@gmail.com>
2024-06-10 20:51:59 -0300

    feat: aula de classes

---
+++ 02-poo-basica-em-java/ClassesNaoExecutaveis/src/Animais/Cachorro.java
@@ -0,0 +1,11 @@
+package Animais;
+
+public class Cachorro {
+
+    public String nome;
+    public int idade;
+    public String cor;
+    public double altura;
+    public double peso;
+    public String raca;
+}
---
+++ 02-poo-basica-em-java/ClassesNaoExecutaveis/src/Main.java
@@ -0,0 +1,12 @@
+import Animais.Cachorro;
+
+public class Main {
+    public static void main(String[] args) {
+        Cachorro c1 = new Cachorro();
+        c1.nome = "Rex";
+        c1.cor = "marrom";
+        c1.altura = 25;
+        c1.peso = 5.5;
+        c1.idade = 2;
+    }
+}
