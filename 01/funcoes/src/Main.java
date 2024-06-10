3fafab20c16e7be814c81623658807bdbbc73f29
LXSCA <lucas.edr27@gmail.com>
2024-06-10 20:25:41 -0300

    fim do primeiro modulo

--- 01/funcoes/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(divide(15, 2));
-    }
-
-    public static float divide(float num1, float num2) {
-        return num1 / num2;
-    }
-}
