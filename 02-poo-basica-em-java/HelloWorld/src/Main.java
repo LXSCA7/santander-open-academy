ec535d5246e0228dcbfc9db2259df7c814975799
LXSCA <lucas.edr27@gmail.com>
2024-06-10 20:34:12 -0300

    feat: hello world e variaveis primitivas em java

---
+++ 02-poo-basica-em-java/HelloWorld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte variavelByte = 120;
+        System.out.println(variavelByte);
+    }
+}
