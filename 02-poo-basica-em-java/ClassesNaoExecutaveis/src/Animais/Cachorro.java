455635d533a5c3a070734ee333d486a4eccc3ab7
LXSCA <lucas.edr27@gmail.com>
2024-06-11 13:23:19 -0300

    feat: Aula sobre encapsulamento

--- 02-poo-basica-em-java/ClassesNaoExecutaveis/src/Animais/Cachorro.java
+++ 02-poo-basica-em-java/ClassesNaoExecutaveis/src/Animais/Cachorro.java
@@ -2,15 +2,81 @@ package Animais;
 
 public class Cachorro {
 
-    public String nome;
-    public int idade;
-    public String cor;
-    public double altura;
-    public double peso;
-    public String raca;
-    public String estadoDeEspirito;
+    private String nome;
+    private int idade;
+    private String cor;
+    private double altura;
+    private double peso;
+    private String raca;
+    private String estadoDeEspirito;
 
-    public void comer() {}
+    public Cachorro() {}
+    public Cachorro(String nome, String cor, double altura, double peso, String raca, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.raca = raca;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    private void comer() {}
     public void latir() {
         System.out.println("Au!");
     }
@@ -20,14 +86,20 @@ public class Cachorro {
     }
 
     public String interagir(String acao) {
-        if (acao.equals("carinho")) {
-            this.estadoDeEspirito = "feliz";
-        } else if (acao.equals("vai dormir")) {
-            this.estadoDeEspirito = "dormindo";
-        } else {
-            this.estadoDeEspirito = "neutro";
+        switch (acao) {
+            case "carinho":
+                this.estadoDeEspirito = "feliz";
+                break;
+            case "vai dormir":
+                this.estadoDeEspirito = "dormindo";
+                break;
+            case "pisar na patinha":
+                this.estadoDeEspirito = "triste";
+                break;
+            default:
+                this.estadoDeEspirito = "neutro";
+                break;
         }
-
         return estadoDeEspirito;
     }
 }
--- 02-poo-basica-em-java/ClassesNaoExecutaveis/src/Main.java
+++ 02-poo-basica-em-java/ClassesNaoExecutaveis/src/Main.java
@@ -3,16 +3,21 @@ import Animais.Cachorro;
 public class Main {
     public static void main(String[] args) {
         Cachorro c1 = new Cachorro();
-        c1.nome = "Rex";
-        c1.cor = "marrom";
-        c1.altura = 25;
-        c1.peso = 5.5;
-        c1.idade = 2;
+        c1.setNome("Rex");
+        c1.setCor("marrom");
+        c1.setAltura(25);
+        c1.setPeso(5.5);
+        c1.setIdade(2);
+
+        Cachorro c2 = new Cachorro("Puppy", "branco", 13, 4.5, "golden", "nada");
 
         c1.latir();
         System.out.println("O cachorro pegou a " + c1.pegar());
         System.out.println("O cachorro está " + c1.interagir("carinho"));
         System.out.println("O cachorro está " + c1.interagir("nada"));
         System.out.println("O cachorro está " + c1.interagir("vai dormir"));
+
+        System.out.println(c2.getNome());
+        System.out.println(c2.getPeso());
     }
 }
